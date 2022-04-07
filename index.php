028092b73902a23a2b3971d962bfddb0a3689c5a
MattFofo <matteo.segattini2@gmail.com>
2022-04-07 17:36:51 +0200

    stile

--- index.php
+++ index.php
@@ -6,28 +6,41 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>PHP Badwords</title>
     </head>
-    <body>
-        <?php
-            $word = $_GET["word"];
-            $text = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt mollitia labore accusantium, omnis aspernatur qui voluptatum hic, voluptates, rerum numquam repellendus quia earum consequatur. Repellendus libero praesentium ut perspiciatis sapiente?';
-        ?>
-        <!-- form per prendere input utente e trasmetterlo -->
-        <form action="index.php" method="GET">
-            <input type="text" name="word">
-            <input type="submit">
-        </form>
+    <body style="display: flex; justify-content: center; align-items: center; ">
+        <div class="container" style="display: flex; justify-content: center; align-items: center; flex-direction: column; max-width: 900px;">
+            <?php
+                $word = $_GET["word"];
+                $text = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt mollitia labore accusantium, omnis aspernatur qui voluptatum hic, voluptates, rerum numquam repellendus quia earum consequatur. Repellendus libero praesentium ut perspiciatis sapiente?';
+            ?>
+            <!-- form per prendere input utente e trasmetterlo -->
+            <form action="index.php" method="GET" style="background-color: lightpink; width: 100%; text-align: center; padding: 2rem">
+                Inserisci parola da censurare <input type="text" name="word">
+                <input type="submit">
+            </form>
 
-        <!-- stampo parola ottenuta dal GET -->
-        <h1><?php echo 'parola da censurare: ' . htmlspecialchars($word) ?></h1>
+            <div style="background-color: lightblue; width: 100%; padding: 2rem">
+                <!-- stampo parola ottenuta dal GET -->
+                <h3><?php echo 'parola da censurare: ' . htmlspecialchars($word) ?></h3>
 
-        <!-- stampo variabile -->
-        <p><?php echo $text ?></p>
+            </div>
 
-        <!-- stampo lunghezza paragrafo -->
-        <h3><?php echo 'lunghezza paragrafo: ' . strlen($text) ?></h3>
+            <div style="background-color: lightcyan; padding: 2rem; width: 100%;">
+                <h2>testo da censurare</h2>
+                <!-- stampo testo -->
+                <p><?php echo $text ?></p>
 
-        <!-- ristampo variabile sostituendo la parola ottenuta dal GET -->
-        <p><?php echo str_replace(htmlspecialchars(strtolower($word)), '***', strtolower($text)) ?></p>
+                <!-- stampo lunghezza paragrafo -->
+                <h3><?php echo 'lunghezza paragrafo: ' . strlen($text) ?></h3>
+            </div>
+
+            <div style="background-color: lightgreen; padding: 2rem; width: 100%;">
+                <h2>testo censurato</h2>
+     
+                 <!-- ristampo testo sostituendo la parola ottenuta dal GET -->
+                <p><?php echo str_replace(htmlspecialchars(strtolower($word)), '***', strtolower($text)) ?></p>
+            </div>   
+
+        </div>
 
     </body>
 </html>
