aab04ce2db5fdf9f1b43c68fbb3b632f683b0f74
MattFofo <matteo.segattini2@gmail.com>
2022-04-07 17:38:55 +0200

    allungato testo

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
         <div class="container" style="display: flex; justify-content: center; align-items: center; flex-direction: column; max-width: 900px;">
             <?php
                 $word = $_GET["word"];
-                $text = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt mollitia labore accusantium, omnis aspernatur qui voluptatum hic, voluptates, rerum numquam repellendus quia earum consequatur. Repellendus libero praesentium ut perspiciatis sapiente?';
+                $text = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt mollitia labore accusantium, omnis aspernatur qui voluptatum hic, voluptates, rerum numquam repellendus quia earum consequatur. Repellendus libero praesentium ut perspiciatis sapiente? Lorem ipsum dolor sit amet consectetur adipisicing elit. In laudantium, numquam vitae aspernatur ex perspiciatis sequi nostrum necessitatibus alias iusto ipsa illum neque odio quaerat, accusamus aut omnis quos error! Lorem ipsum dolor, sit amet consectetur adipisicing elit. Molestiae sit quam, cum eaque obcaecati, quaerat maxime rem, placeat ab neque officiis pariatur aliquam fugit ullam minus quidem assumenda reprehenderit nam. Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa expedita id deserunt recusandae assumenda rerum atque voluptatibus, similique odit! Suscipit libero blanditiis aliquid necessitatibus quam laboriosam corporis, delectus est fuga!';
             ?>
             <!-- form per prendere input utente e trasmetterlo -->
             <form action="index.php" method="GET" style="background-color: lightpink; width: 100%; text-align: center; padding: 2rem">
